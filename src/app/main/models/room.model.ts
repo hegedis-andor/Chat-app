c6b35463ded456ce0905d625a1cb7ed1c39bbcf8
hegedis.andor <hegedis.andor@gmail.com>
2019-07-15 13:18:56 +0200

    c

--- src/app/main/models/room.model.ts
+++ src/app/main/models/room.model.ts
@@ -1,7 +1,7 @@
 export interface Room {
     key?: string;
-    name: string;
-    accessibility: string;
-    password: string;
+    name?: string;
+    accessibility?: string;
+    password?: string;
     createdBy?: string;
 }
